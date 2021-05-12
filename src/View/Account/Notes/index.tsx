572c9975fd2b9210ff7942bc98913e68182cd685
Dariia <phoenixstryi@gmail.com>
2021-05-12 12:24:42 +0300

    some fixes

--- src/View/Account/Notes/index.tsx
+++ src/View/Account/Notes/index.tsx
@@ -8,7 +8,7 @@ interface IProps {}
 const Notes: React.FC<IProps> = () => {
   return (
     <div className={'notes'}>
-      <NavigationBar rout={'account'} name={'My Notes'} />
+      <NavigationBar rout={'account'} name={'My Notes'} hasSaveButton={false}/>
     </div>
   );
 };
