a7d21cac246d53ef1fcbcaee240dc04530f253e5
like-me4 <andretanforever@gmail.com>
2021-06-21 14:08:01 +0300

    changes

--- src/utils/checkCommonItems.ts
+++
@@ -1,4 +0,0 @@
-export const checkCommonItems = function<type = string>(arr1: type[], arr2: type[]){
-  if(arr2.length === 0) return true;
-  return arr1.some(item => arr2.includes(item))
-};
--- src/utils/navigationButtonsStyler.ts
+++
@@ -1,3 +0,0 @@
-export const styleBTNSetter = (name: string) => {
-    console.log('styled');
-}
