5ec18fe390cb6419fd87ca9f7d6ce606caab8cd2
Dariia <phoenixstryi@gmail.com>
2021-07-22 16:02:38 +0300

    updated journey styles

--- src/view/Journey/CheckoutBody.tsx
+++ src/view/Journey/CheckoutBody.tsx
@@ -12,36 +12,38 @@ interface IProps {
 
 const CheckoutBody: React.FC<IProps> = ({ ...props }) => {
   return (
-    <div className={'checkout-body'}>
-      <div className={'checkout-body-img-wrap'}>
-        <img src={props.img} alt='' className='checkout-body-img' />
-      </div>
-      <div className={'checkout-body-title'}>
+    <div className='checkout-body-wrap'>
+      <div className={'checkout-body'}>
+        <div className={'checkout-body-img-wrap'}>
+          <img src={props.img} alt='' className='checkout-body-img' />
+        </div>
+        <div className={'checkout-body-title'}>
         <span className={'checkout-body-title-text'}>
           {props.title}
         </span>
-      </div>
-      <div className='checkout-body-timing'>
+        </div>
+        <div className='checkout-body-timing'>
         <span className='checkout-body-timing-hours'>
           {props.duration === 0 ? `Endless` : `${Math.round(props.duration / 60)} hrs`}
         </span>
-        <span className='checkout-body-timing-text'>duration</span>
-      </div>
-      {props.minDaySpent || props.maxDaySpent ? (
-        <div className='checkout-body-timing'>
+          <span className='checkout-body-timing-text'>duration</span>
+        </div>
+        {props.minDaySpent || props.maxDaySpent ? (
+          <div className='checkout-body-timing'>
         <span className='checkout-body-timing-hours'>
           {props.minDaySpent && props.maxDaySpent ?
             (`${Math.round(props.minDaySpent / 60)} - ${Math.round(props.maxDaySpent / 60)}`) :
             (props.minDaySpent ? (Math.round(props.minDaySpent / 60)) : (Math.round(props.maxDaySpent / 60)))}
         </span>
-          <span className='checkout-body-timing-text'>hrs of effort per day</span>
-        </div>
-      ) : (<></>)}
-      <div className='checkout-body-price'>
+            <span className='checkout-body-timing-text'>hrs of effort per day</span>
+          </div>
+        ) : (<></>)}
+        <div className='checkout-body-price'>
         <span className='checkout-body-price-number'>
           $ {props.price}
         </span>
-        <span className='checkout-body-price-text'>price</span>
+          <span className='checkout-body-price-text'>price</span>
+        </div>
       </div>
     </div>
   )
