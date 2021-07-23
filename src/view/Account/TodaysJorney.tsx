aabadf0dbcb040db65fc71ba97f692db882d86e7
Dariia <phoenixstryi@gmail.com>
2021-07-23 11:16:52 +0300

    updated account styles

--- src/view/Account/TodaysJorney.tsx
+++ src/view/Account/TodaysJorney.tsx
@@ -39,7 +39,7 @@ const TodaysJourney: React.FC<any> = ({ ...props }) => {
         <div className={'profile-journey'}>
           <div className={'profile-journey-progress'}>
             <span className={'profile-journey-progress-header'}>
-              Todays progress
+              Today's progress
             </span>
             <div className={'profile-journey-progress-wrapper'}>
               <div className={'profile-journey-progress__left'}>
@@ -99,7 +99,7 @@ const TodaysJourney: React.FC<any> = ({ ...props }) => {
                     className={
                       'profile-journey-progress__right-textwrapper__top'
                     }>
-                    {statistic.today.completedTaskCount}/
+                    {statistic.today.completedTaskCount}{' '}/{' '}
                     {statistic.today.maxTaskCount}
                   </span>
                   <span
